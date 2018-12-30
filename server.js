29020347e14a7721b47f65c4e370f5fe171bd2c8
eolculnamo2 <rbertram8@gmail.com>
2018-12-30 14:13:29 -0600

    *

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('dist'));
 
-app.get('/', (req,res) => res.sendFile(__dirname+'/public/index.html'));
+app.get('*', (req,res) => res.sendFile(__dirname+'/public/index.html'));
 
 app.listen(8080, () => console.log("Server running on port 8080"));
