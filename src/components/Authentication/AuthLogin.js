e73fe71cbfcf904c44f2dfd8cc5752f358a2caac
briansterling93 <41628472+briansterling93@users.noreply.github.com>
2019-05-01 22:24:28 -0700

    styling changes

--- src/components/Authentication/AuthLogin.js
+++ src/components/Authentication/AuthLogin.js
@@ -3,21 +3,26 @@ import React from 'react';
 const AuthLogin = () => (
     <div className='introPage'>
     <div className='introContainer'>
-            <h2>Organize Your Collection :D</h2>
+            <h1 className='introTitle'>Organize Your Library</h1>
             <ul>
-            <li>Locate books with book search</li>
-            <li>Give each read a rating</li>
-            <li>Each of your reads are directly linked to google purchase</li>
+            <li>Find books with book search</li>
+            <li>Rate your books</li>
+            <li>Books link to google books purchase option</li>
+            <li>Find books with book search</li>
             </ul>
     </div>
     <div className='loginContainer'>
+            <h1 id='loginTitle'>Login</h1>
             <form>
-            <p>Username: <input placeholder='enter username'/></p>
-            <p>Password: <input placeholder='enter password'/></p>
-            <a href='' className='fPw'>Forgot Password?</a>
-
-            <button className='loginBtn'>Submit</button>
+            <p>Username <br /><input className='inputs'/></p>
+            <p>Password <br /><input className='inputs'/></p>
+            <div className='signInArea'><a href='' className='fPw'>Forgot Password?</a>
+        <br />
+        <br />
+            <button className='loginBtn'>Login</button>
+            </div>
             </form>
+            <p id='loginCreate'>Don't have an account? <a href=''>Create Account?</a></p>
     </div>
     </div>
 );
--- src/components/Authentication/AuthTitle.js
+++ src/components/Authentication/AuthTitle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AuthTitle = () => (
-    <div>
+    <div id='title'>
         <h1 className='title'>BookShelf</h1>
     </div>
 );
